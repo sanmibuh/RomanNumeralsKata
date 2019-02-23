ee81d343a5d7a8d71f99e11362744621bb067069
David Belmez Vicente <sanmibuh@gmail.com>
2019-02-23 19:51:36 +0100

    [ARITZ-BELMEZ]: Case 3 refactor

--- src/main/java/org/sanmibuh/kata/roman/NumberConverter.java
+++ src/main/java/org/sanmibuh/kata/roman/NumberConverter.java
@@ -3,12 +3,11 @@ package org.sanmibuh.kata.roman;
 public class NumberConverter {
 
   public String toRoman(final int number) {
-    if(2 == number) {
-      return "II";
+    final StringBuilder roman = new StringBuilder();
+    for (int i = 1; i <= number; i++) {
+      roman.append("I");
     }
-    if (3 == number) {
-      return "III";
-    }
-    return "I";
+    return roman.toString();
   }
+
 }
