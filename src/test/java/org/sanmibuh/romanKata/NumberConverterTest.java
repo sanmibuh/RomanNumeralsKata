7376758a6ed5216eb7d0cf142628b199be404dfa
David Belmez Vicente <sanmibuh@gmail.com>
2019-02-23 19:21:17 +0100

    [INIT]: Init Kata

---
+++ src/test/java/org/sanmibuh/romanKata/NumberConverterTest.java
@@ -0,0 +1,13 @@
+package org.sanmibuh.romanKata;
+
+import org.junit.Before;
+
+public class NumberConverterTest {
+
+  private NumberConverter numberConverter;
+
+  @Before
+  public void setUp() throws Exception {
+    numberConverter = new NumberConverter();
+  }
+}
