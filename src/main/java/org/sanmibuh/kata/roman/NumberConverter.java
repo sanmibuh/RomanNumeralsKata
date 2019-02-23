804fefeb33bffa7fdc52e09d6a5ee7ade8766892
David Belmez Vicente <sanmibuh@gmail.com>
2019-02-23 20:00:28 +0100

    [ARITZ-BELMEZ]: Refactor Case 7

--- src/main/java/org/sanmibuh/kata/roman/NumberConverter.java
+++ src/main/java/org/sanmibuh/kata/roman/NumberConverter.java
@@ -4,17 +4,13 @@ public class NumberConverter {
 
   public String toRoman(final int number) {
     final StringBuilder roman = new StringBuilder();
-    if(5 == number) {
-      return "V";
-    }
-    if(6 == number) {
-      return "VI";
-    }
-    if(7 == number) {
-      return "VII";
-    }
-    for(int i = 1; i <= number; i++) {
-      roman.append("I");
+    if(number >= 5) {
+      roman.append("V");
+      roman.append(toRoman(number - 5));
+    }else {
+      for (int i = 1; i <= number; i++) {
+        roman.append("I");
+      }
     }
     return roman.toString();
   }
