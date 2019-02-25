1146f1f84029d2714e0173a9ea41596f80073b9f
David Belmez Vicente <sanmibuh@gmail.com>
2019-02-25 09:43:19 +0100

    [TRY-1]: Refactor

--- src/main/java/org/sanmibuh/kata/roman/NumberConverter.java
+++ src/main/java/org/sanmibuh/kata/roman/NumberConverter.java
@@ -1,27 +1,34 @@
 package org.sanmibuh.kata.roman;
 
+import java.util.Optional;
+import java.util.stream.Stream;
+
 public class NumberConverter {
 
+  private enum RomanSymbol {
+    I(1),
+    V(5),
+    X(10),
+    L(50),
+    C(100),
+    D(500),
+    M(1000);
+
+    private final int value;
+
+    RomanSymbol(final int value) {
+      this.value = value;
+    }
+
+    public static Optional<RomanSymbol> fromValue(final int value) {
+      return Stream.of(RomanSymbol.values())
+          .filter(romanSymbol -> romanSymbol.value == value)
+          .findFirst();
+    }
+  }
+
   public String toRoman(final int number) {
-    if (number == 5) {
-      return "V";
-    }
-    if (number == 10) {
-      return "X";
-    }
-    if (number == 50) {
-      return "L";
-    }
-    if (number == 100) {
-      return "C";
-    }
-    if (number == 500) {
-      return "D";
-    }
-    if (number == 1000) {
-      return "M";
-    }
-    return "I";
+    return RomanSymbol.fromValue(number).map(RomanSymbol::name).orElse(null);
   }
 
 }
