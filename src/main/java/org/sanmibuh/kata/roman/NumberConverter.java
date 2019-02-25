07db67e95ec54562395ce2eae1f1a3fa7cc247f5
David Belmez Vicente <sanmibuh@gmail.com>
2019-02-25 13:00:44 +0100

    [TRY-1]: Refactor

--- src/main/java/org/sanmibuh/kata/roman/NumberConverter.java
+++ src/main/java/org/sanmibuh/kata/roman/NumberConverter.java
@@ -7,49 +7,56 @@ public class NumberConverter {
 
   private enum RomanSymbol {
     I(1),
-    V(5),
+    V(5, true),
     X(10),
-    L(50),
+    L(50, true),
     C(100),
-    D(500),
+    D(500, true),
     M(1000);
 
     private final int value;
+    private final boolean by5;
 
     RomanSymbol(final int value) {
+      this(value, false);
+    }
+
+    RomanSymbol(final int value, final boolean by5) {
       this.value = value;
+      this.by5 = by5;
     }
   }
 
   public String toRoman(final int number) {
     final StringBuilder romanNumber = new StringBuilder();
-    if (number == 4) {
-      return "IV";
-    }
-    if (number == 9) {
-      return "IX";
-    }
-    if (number == 40) {
-      return "XL";
-    }
-    if (number == 90) {
-      return "XC";
-    }
-    if (number == 400) {
-      return "CD";
-    }
-    if (number == 900) {
-      return "CM";
-    }
     Stream.of(RomanSymbol.values())
         .sorted(Collections.reverseOrder())
         .filter(romanSymbol -> number / romanSymbol.value > 0)
         .findFirst().ifPresent(romanSymbol -> {
       final int times = number / romanSymbol.value;
-      for (int i = 0; i < times; i++) {
+      int substraction = 0;
+      if (times == 4) {
         romanNumber.append(romanSymbol);
+        romanNumber.append(RomanSymbol.values()[romanSymbol.ordinal() + 1]);
+        substraction = romanSymbol.value * times;
+      } else if (romanSymbol.by5) {
+        final RomanSymbol previousRomanSymbol = RomanSymbol.values()[romanSymbol.ordinal() - 1];
+        final RomanSymbol nextRomanSymbol = RomanSymbol.values()[romanSymbol.ordinal() + 1];
+        if (number == nextRomanSymbol.value - previousRomanSymbol.value) {
+          romanNumber.append(previousRomanSymbol);
+          romanNumber.append(RomanSymbol.values()[previousRomanSymbol.ordinal() + 2]);
+          substraction = previousRomanSymbol.value * 9;
+        } else {
+          romanNumber.append(romanSymbol);
+          substraction = romanSymbol.value;
+        }
+      } else {
+        for (int i = 0; i < times; i++) {
+          romanNumber.append(romanSymbol);
+          substraction += romanSymbol.value;
+        }
       }
-      romanNumber.append(toRoman(number - (romanSymbol.value * times)));
+      romanNumber.append(toRoman(number - substraction));
     });
 
     return romanNumber.toString();
